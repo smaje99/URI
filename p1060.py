76c2b1ca9ab45ab3959ecdcdd8f1fd82ae15a1f7
Reymon99 <s.maje@udla.edu.co>
2018-08-04 17:05:37 -0500

    URI's exercise 1060

---
+++ p1060.py
@@ -0,0 +1,12 @@
+a=float(input())
+b=float(input())
+c=float(input())
+d=float(input())
+e=float(input())
+f=float(input())
+i=0
+l=[a,b,c,d,e,f]
+for a in l:
+    if a>=0:
+        i+=1
+print(i,"valores positivos")
