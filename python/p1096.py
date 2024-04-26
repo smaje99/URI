7b765e9119fb610e2032548a0bd9fdeaae302d6e
Sergio Majé <smajefranco@gmail.com>
2024-04-26 12:00:00 -0500

    ✨ Beecrowd exercise 1096 in Python

---
+++ python/p1096.py
@@ -0,0 +1,18 @@
+"""Beecrowd exercise 1096.
+
+See: https://judge.beecrowd.com/es/problems/view/1096
+"""
+
+
+def main():
+    """Main function."""
+    i_iter = range(1, 10)
+    j_iter = range(7, 4, -1)
+
+    for i in i_iter:
+        for j in j_iter:
+            print(f"I={i} J={j}")
+
+
+if __name__ == "__main__":
+    main()
