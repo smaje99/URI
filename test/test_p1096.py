7fb9916cc38d248468020a3a3399c0f321bfba9f
Sergio Majé <smajefranco@gmail.com>
2024-04-27 12:00:00 -0500

    ✅ Add test for Beecrowd exercise 1096 in Python

---
+++ test/test_p1096.py
@@ -0,0 +1,44 @@
+"""Test for Beecrowd exercise 1096."""
+
+import pytest
+from pytest import CaptureFixture
+
+from python.p1096 import main as p1096
+
+
+@pytest.mark.parametrize("expected_output", [(
+  'I=1 J=7\n'
+  'I=1 J=6\n'
+  'I=1 J=5\n'
+  'I=2 J=7\n'
+  'I=2 J=6\n'
+  'I=2 J=5\n'
+  'I=3 J=7\n'
+  'I=3 J=6\n'
+  'I=3 J=5\n'
+  'I=4 J=7\n'
+  'I=4 J=6\n'
+  'I=4 J=5\n'
+  'I=5 J=7\n'
+  'I=5 J=6\n'
+  'I=5 J=5\n'
+  'I=6 J=7\n'
+  'I=6 J=6\n'
+  'I=6 J=5\n'
+  'I=7 J=7\n'
+  'I=7 J=6\n'
+  'I=7 J=5\n'
+  'I=8 J=7\n'
+  'I=8 J=6\n'
+  'I=8 J=5\n'
+  'I=9 J=7\n'
+  'I=9 J=6\n'
+  'I=9 J=5\n'
+)])
+def test_exercise_1096(capfd: CaptureFixture[str], expected_output: str):
+    """Test for Beecrowd exercise 1096."""
+    p1096()
+
+    captured = capfd.readouterr()
+
+    assert captured.out == expected_output
