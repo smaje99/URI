f49bdbe3842dbcddcbef2ad57064f809a0950a22
Sergio Majé <smajefranco@gmail.com>
2024-05-27 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1015 in Pyhon

--- python/p1015.py
+++ python/p1015.py
@@ -1,8 +1,11 @@
+'''Beecrowd exercise 1015.
+
+See: https://judge.beecrowd.com/es/problems/view/1015
+'''
+
 import math
-x1,y1=input().split()
-x2,y2=input().split()
-x1=float(x1)
-x2=float(x2)
-y1=float(y1)
-y2=float(y2)
-print("{:0.4f}".format(math.sqrt((x2-x1)**2+(y2-y1)**2)))
+
+x1, y1 = map(float, input().split())
+x2, y2 = map(float, input().split())
+
+print(f"{math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2):0.4f}")
