b630efea83a1dcb09df4a4b0911d37c317b221f3
Sergio Majé <smajefranco@gmail.com>
2024-05-29 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1017 in Pyhon

--- python/p1017.py
+++ python/p1017.py
@@ -1,3 +1,9 @@
-a=int(input())
-b=int(input())
-print("{:0.3f}".format((a*b)/12))
+'''Beecrowd exercise 1017.
+
+See: https://judge.beecrowd.com/es/problems/view/1017
+'''
+
+a = int(input())
+b = int(input())
+
+print(f'{(a * b) / 12:.3f}')
