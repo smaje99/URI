4c2b9aebca03c263d6f3fed082756003a3e95f3a
Sergio Majé <smajefranco@gmail.com>
2024-05-31 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1019 in Pyhon

--- python/p1019.py
+++ python/p1019.py
@@ -1,2 +1,7 @@
-s=int(input())
-print(repr((s//60)//60)+":"+repr((s//60)%60)+":"+repr(s%60))
+'''Beecrowd exercise 1019.
+
+See: https://judge.beecrowd.com/es/problems/view/1019
+'''
+
+s = int(input())
+print(repr((s // 60) // 60) + ":" + repr((s // 60) % 60) + ":" + repr(s % 60))
