370cda070c9c246ad71f4a47e73c6fa886a45a73
Sergio Majé <smajefranco@gmail.com>
2024-06-01 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1020 in Pyhon

--- python/p1020.py
+++ python/p1020.py
@@ -1,4 +1,10 @@
-a=int(input())
-print((a//365),"ano(s)")
-print(((a%365)//30),"mes(es)")
-print(((a%365)%30),"dia(s)")
+'''Beecrowd exercise 1020.
+
+See: https://judge.beecrowd.com/es/problems/view/1020
+'''
+
+a = int(input())
+
+print((a // 365), "ano(s)")
+print(((a % 365) // 30), "mes(es)")
+print(((a % 365) % 30), "dia(s)")
