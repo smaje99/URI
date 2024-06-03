ed5944e682d67619972113249dd4d2a55b074765
Sergio Majé <smajefranco@gmail.com>
2024-06-03 12:00:00 -0500

    ♻️ Refactor solution for Beecrowd exercise 1021 in Pyhon

--- python/p1021.py
+++ python/p1021.py
@@ -1,16 +1,22 @@
-n=float(input())
+"""Beecrowd exercise 1021.
+
+See: https://judge.beecrowd.com/es/problems/view/1021
+"""
+
+notas = [100.0, 50.0, 20.0, 10.0, 5.0, 2.0]
+moedas = [1.0, 0.5, 0.25, 0.1, 0.05, 0.01]
+
+n = float(input())
+valor = (n * 100) + 0.05
+
 print("NOTAS:")
-x=int(n)
-print((x//100),"nota(s) de R$ 100.00")
-print(((x%100)//50),"nota(s) de R$ 50.00")
-print((((x%100)%50)//20),"nota(s) de R$ 20.00")
-print(((((x%100)%50)%20)//10),"nota(s) de R$ 10.00")
-print((((((x%100)%50)%20)%10)//5),"nota(s) de R$ 5.00")
-print(((((((x%100)%50)%20)%10)%5)//2),"nota(s) de R$ 2.00")
+for nota in notas:
+    qtd = int(valor // (nota * 100))
+    print(f"{qtd} nota(s) de R$ {nota:.2f}")
+    valor %= nota * 100
+
 print("MOEDAS:")
-print((((((((x%100)%50)%20)%10)%5)%2)//1),"moeda(s) de R$ 1.00")
-print(int((((((((n%100)%50)%20)%10)%5)%2)%1)/0.5),"moeda(s) de R$ 0.50")
-print(int(((((((((n%100)%50)%20)%10)%5)%2)%1)%0.5)/0.25),"moeda(s) de R$ 0.25")
-print(int((((((((((n%100)%50)%20)%10)%5)%2)%1)%0.5)%0.25)/0.1),"moeda(s) de R$ 0.10")
-print(int(((((((((((n%100)%50)%20)%10)%5)%2)%1)%0.5)%0.25)%0.1)/0.05),"moeda(s) de R$ 0.05")
-print("{:0.0f}".format((((((((((((n%100)%50)%20)%10)%5)%2)%1)%0.5)%0.25)%0.1)%0.05)/0.01),"moeda(s) de R$ 0.01")
+for moeda in moedas:
+    qtd = int(valor // (moeda * 100))
+    print(f"{qtd} moeda(s) de R$ {moeda:.2f}")
+    valor %= moeda * 100
