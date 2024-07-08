219fc0a8992a1447e8e5bce000ffa4f7d8baccd5
Sergio Majé <smajefranco@gmail.com>
2024-07-08 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1026 in Python

--- python/p1026.py
+++ python/p1026.py
@@ -1,8 +1,15 @@
-l=[]
+"""Beecrowd exercise 1026.
+
+See: https://judge.beecrowd.com/es/problems/view/1026
+"""
+
+
+xors: list[int] = []
+
 try:
     while True:
-        a,b=input().split()
-        l.append(int(a)^int(b))
-except EOFError as error:
-    for i in l:
+        a, b = map(int, input().split())
+        xors.append(a ^ b)
+except EOFError:
+    for i in xors:
         print(i)
