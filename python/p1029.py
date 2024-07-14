4153c2d081e4bff14b7f74baf955b5ac6b42d986
Sergio Majé <smajefranco@gmail.com>
2024-07-14 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1029 in Python

--- python/p1029.py
+++ python/p1029.py
@@ -1,16 +1,23 @@
-def fib(n):
-    f = [0] * (n + 1)
-    cl = [0] * (n + 1)
+"""Beecrowd exercise 1029.
+
+See: https://judge.beecrowd.com/es/problems/view/1029
+"""
+
+
+def fib(num: int) -> tuple[int, int]:
+    """Calculate the n-th Fibonacci number and the number of calls."""
+    f = [0] * (num + 1)
+    cl = [0] * (num + 1)
     f[1] = 1
     cl[1] = 0
-    for i in range(2, n + 1):
+    for i in range(2, num + 1):
         f[i] = f[i - 1] + f[i - 2]
         cl[i] = cl[i - 1] + cl[i - 2] + 2
-    return cl[n], f[n]
+    return cl[num], f[num]
 
 
 T = int(input())
 for _ in range(T):
     n = int(input())
     c, r = fib(n)
-    print("fib(%d) = %d calls = %d" % (n, c, r))
+    print(f"fib({n}) = {c} calls = {r}")
