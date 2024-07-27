15650ad3f2cd406d9f2028c9b4eee6a64538641b
Sergio Majé <smajefranco@gmail.com>
2024-07-27 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1036 in Python

--- python/p1036.py
+++ python/p1036.py
@@ -1,10 +1,16 @@
+"""Beecrowd exercise 1036.
+
+See: https://judge.beecrowd.com/es/problems/view/1036
+"""
+
 import math
-a,b,c=input().split()
-a=float(a)
-b=float(b)
-c=float(c)
+
+a, b, c = map(float, input().split())
+
 try:
-    print("R1 = {:0.5f}".format(((b*-1)+math.sqrt(((b**2)-(4*a*c))))/(2*a)))
-    print("R2 = {:0.5f}".format(((b*-1)-math.sqrt(((b**2)-(4*a*c))))/(2*a)))
-except:
+    r1 = ((b * -1) + math.sqrt(((b**2) - (4 * a * c)))) / (2 * a)
+    r2 = ((b * -1) - math.sqrt(((b**2) - (4 * a * c)))) / (2 * a)
+
+    print(f"R1 = {r1:0.5f}\nR2 = {r2:0.5f}")
+except ValueError:
     print("Impossivel calcular")
