edd8e989beeb514bcc11a94a07bab7bc088d3318
Sergio Majé <smajefranco@gmail.com>
2024-07-30 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1037 in Python

--- python/p1037.py
+++ python/p1037.py
@@ -1,11 +1,23 @@
-n=float(input())
-if 0 < n <= 25.0000:
-    print("Intervalo [0,25]")
-elif 25.0000 < n <= 50.0000:
-    print("Intervalo (25,50]")
-elif 50.0000 < n <= 75.000:
-    print("Intervalo (50,75]")
-elif 75.0000 < n <= 100.0000:
-    print("Intervalo (75,100]")
-else:
-    print("Fora de intervalo")
+"""Beecrowd exercise 1037.
+
+See: https://judge.beecrowd.com/es/problems/view/1037
+"""
+
+intervalos: dict[tuple[int, int], str] = {
+    (0, 25): "Intervalo [0,25]",
+    (25, 50): "Intervalo (25,50]",
+    (50, 75): "Intervalo (50,75]",
+    (75, 100): "Intervalo (75,100]",
+}
+
+
+def find_intervalo(number: float) -> str:
+    """Determina en qué intervalo se encuentra un número dado."""
+    for (inicio, fin), mensaje in intervalos.items():
+        if inicio <= number <= fin:
+            return mensaje
+    return "Fora de intervalo"
+
+
+n = float(input())
+print(find_intervalo(n))
