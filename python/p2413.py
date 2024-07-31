4f1cb29c8ec476a71aa267105a1c244455f0beda
Sergio Majé <smajefranco@gmail.com>
2024-07-31 12:00:00 -0500

    ✨ Add solution for Beecrowd exercise 2413 in Python

---
+++ python/p2413.py
@@ -0,0 +1,15 @@
+"""Beecrowd exercise 2413.
+
+See: https://judge.beecrowd.com/es/problems/view/2413
+"""
+
+
+def main():
+    """Main function."""
+    t_link = int(input())
+
+    print(t_link * 4)
+
+
+if __name__ == "__main__":
+    main()
