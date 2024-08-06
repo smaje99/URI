d2697c44d8db8a3e117a41a1c151ecbfeaff3205
Sergio Majé <smajefranco@gmail.com>
2024-08-06 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1044 in Python

--- python/p1044.py
+++ python/p1044.py
@@ -1,2 +1,12 @@
-a,b=input().split()
-print("Sao Multiplos" if int(a)%int(b)==0 or int(b)%int(a)==0 else "Nao sao Multiplos")
+"""Beecrowd exercise 1044.
+
+See: https://judge.beecrowd.com/es/problems/view/1044
+"""
+
+a, b = map(int, input().split())
+
+print(
+    "Sao Multiplos"
+    if a % b == 0 or b % a == 0
+    else "Nao sao Multiplos"
+)
