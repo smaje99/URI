117548c8be6d12a49365ce7264c035e1814476ca
Sergio Majé <smajefranco@gmail.com>
2024-08-07 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1045 in Python

--- python/p1045.py
+++ python/p1045.py
@@ -1,19 +1,28 @@
-A,B,C=input().split()
-tupla=[float(A),float(B),float(C)]
-tupla.sort()
-A=tupla[2]
-B=tupla[1]
-C=tupla[0]
-if A>=B+C:
+"""Beecrowd exercise 1045.
+
+See: https://judge.beecrowd.com/es/problems/view/1045
+"""
+
+sides = map(float, input().split())
+
+C, B, A = sorted(sides)
+
+if A >= B + C:
     print("NAO FORMA TRIANGULO")
 else:
-    if A**2 == (B**2)+(C**2):
+    A2 = A ** 2
+    sum_BC2 = B ** 2 + C ** 2
+
+    # Clasificar el triángulo por sus ángulos
+    if A2 == sum_BC2:
         print("TRIANGULO RETANGULO")
-    if A**2 > (B**2)+(C**2):
+    elif A2 > sum_BC2:
         print("TRIANGULO OBTUSANGULO")
-    if A**2 < (B**2)+(C**2):
+    elif A2 < sum_BC2:
         print("TRIANGULO ACUTANGULO")
-    if A==B==C:
+
+    # Clasificar el triángulo por sus lados
+    if A == B == C:
         print("TRIANGULO EQUILATERO")
-    if (A==B!=C) or (B==C!=A) or (C==A!=B):
+    if A == B or B == C or A == C:
         print("TRIANGULO ISOSCELES")
