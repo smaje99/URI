a683957e71f08d0dd75d737bf7496cf37c635ca9
Sergio Majé <smajefranco@gmail.com>
2024-08-10 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1047 in Python

--- python/p1047.py
+++ python/p1047.py
@@ -1,26 +1,23 @@
-a,b,c,d=input().split()
-a=int(a)
-b=int(b)
-c=int(c)
-d=int(d)
-if a==c:
-    if b==d:
-        print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
-    elif b>d:
-        print("O JOGO DUROU 23 HORA(S) E {} MINUTO(S)".format(60-(b-d)))
-    else:
-        print("O JOGO DUROU 0 HORA(S) E {} MINUTO(S)".format(d-b))
-elif c>a:
-    if b==d:
-        print("O JOGO DUROU {} HORA(S) E 0 MINUTO(S)".format(c-a))
-    elif b>d:
-        print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(c-a-1,60-(b-d)))
-    else:
-        print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(c-a,d-b))
-else:
-    if b==d:
-        print("O JOGO DUROU {} HORA(S) E 0 MINUTO(S)".format(24-a+c))
-    elif b>d:
-        print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(24-a+c-1,60-(b-d)))
-    else:
-        print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(24-a+c,d-b))
+"""Beecrowd exercise 1047.
+
+See: https://judge.beecrowd.com/es/problems/view/1047
+"""
+
+a, b, c, d = map(int, input().split())
+
+# Convertir el tiempo de inicio y fin en minutos desde el inicio del día
+start_time = a * 60 + b
+end_time = c * 60 + d
+
+# Si el tiempo de fin es menor o igual al de inicio, cruzamos la medianoche
+if end_time <= start_time:
+    end_time += 24 * 60
+
+# Calcular la duración en minutos
+duration_minutes = end_time - start_time
+
+# Convertir de minutos a horas y minutos
+hours = duration_minutes // 60
+minutes = duration_minutes % 60
+
+print(f"O JOGO DUROU {hours} HORA(S) E {minutes} MINUTO(S)")
