fe8d299cf5eef336e85b7cb2e113b5d2583cce1f
Sergio Majé <smajefranco@gmail.com>
2024-08-27 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1050 in Python

--- python/p1050.py
+++ python/p1050.py
@@ -1,19 +1,19 @@
-a=int(input())
-if a==61:
-    print("Brasilia")
-elif a==71:
-    print("Salvador")
-elif a==11:
-    print("Sao Paulo")
-elif a==21:
-    print("Rio de Janeiro")
-elif a==32:
-    print("Juiz de Fora")
-elif a==19:
-    print("Campinas")
-elif a==27:
-    print("Vitoria")
-elif a==31:
-    print("Belo Horizonte")
-else:
-    print("DDD nao cadastrado")
+"""Beecrowd exercise 1050.
+
+See: https://judge.beecrowd.com/es/problems/view/1050
+"""
+
+a = int(input())
+
+ddd = {
+    61: "Brasilia",
+    71: "Salvador",
+    11: "Sao Paulo",
+    21: "Rio de Janeiro",
+    32: "Juiz de Fora",
+    19: "Campinas",
+    27: "Vitoria",
+    31: "Belo Horizonte"
+}
+
+print(ddd.get(a, "DDD nao cadastrado"))
