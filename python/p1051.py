224ee270be1694d49281b2e930d2f7a96971a7c3
Sergio Majé <smajefranco@gmail.com>
2024-09-07 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1051 in Python

--- python/p1051.py
+++ python/p1051.py
@@ -1,9 +1,21 @@
-imp = float(input())
-if 0 <= imp <= 2000:
-    print('Isento')
-elif 2000.01 <= imp <= 3000:
-    print('R$ {:0.2f}'.format((imp - 2000) * 0.08))
-elif 3000.01 <= imp <= 4500:
-    print('R$ {:0.2f}'.format((1000 * 0.08) + ((imp - 3000) * 0.18)))
+"""Beecrowd exercise 1051.
+
+See: https://judge.beecrowd.com/es/problems/view/1051
+"""
+
+income = float(input())
+
+if 0 <= income <= 2000:
+    print("Isento")
 else:
-    print('R$ {:0.2f}'.format((1000 * 0.08) + (1500 * 0.18) + ((imp - 4500) * 0.28)))
+    tax: float = 0
+    if income > 4500:
+        tax += (income - 4500) * 0.28
+        income = 4500
+    if income > 3000:
+        tax += (income - 3000) * 0.18
+        income = 3000
+    if income > 2000:
+        tax += (income - 2000) * 0.08
+
+    print(f"R$ {tax:.2f}")
