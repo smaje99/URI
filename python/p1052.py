70c7e581c389f014537cd26e4ad581d5b54c5016
Sergio Majé <smajefranco@gmail.com>
2024-09-08 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1052 in Python

--- python/p1052.py
+++ python/p1052.py
@@ -1,25 +1,24 @@
+"""Beecrowd exercise 1052.
+
+See: https://judge.beecrowd.com/es/problems/view/1052
+"""
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
 m = int(input())
-if m == 1:
-    print("January")
-elif m == 2:
-    print("February")
-elif m == 3:
-    print("March")
-elif m == 4:
-    print("April")
-elif m == 5:
-    print("May")
-elif m == 6:
-    print("June")
-elif m == 7:
-    print("July")
-elif m == 8:
-    print("August")
-elif m == 9:
-    print("September")
-elif m == 10:
-    print("October")
-elif m == 11:
-    print("November")
-elif m == 12:
-    print("December")
+
+if 1 <= m <= 12:
+    print(months[m - 1])
