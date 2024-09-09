69b332bce8c98ada990ccbd5495aa9b66e86ff06
Sergio Majé <smajefranco@gmail.com>
2024-09-09 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1059 in Python

--- python/p1059.py
+++ python/p1059.py
@@ -1,3 +1,7 @@
-for i in range(1,101):
-    if i%2==0:
-        print(i)
+"""Beecrowd exercise 1059.
+
+See: https://judge.beecrowd.com/es/problems/view/1059
+"""
+
+for i in range(2, 101, 2):
+    print(i)
