01429c6ac64dae821e8ff03b5198df844d438e05
Sergio Majé <smajefranco@gmail.com>
2024-09-11 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1061 in Python

--- python/p1061.py
+++ python/p1061.py
@@ -1,20 +1,27 @@
+"""Beecrowd exercise 1061.
+
+See: https://judge.beecrowd.com/es/problems/view/1061
+"""
+
+# Leer el primer día y la hora
 dia1 = int(input().split()[1])
-h1, m1, s1, = map(int, input().split(':'))
+h1, m1, s1 = map(int, input().split(':'))
+
+# Leer el segundo día y la hora
 dia2 = int(input().split()[1])
-h2, m2, s2, = map(int, input().split(':'))
-dia = dia2 - dia1
-h = h2 - h1
-if h < 0:
-    h += 24
-    dia -= 1
-m = m2 - m1
-if m < 0:
-    m += 60
-    h -= 1
-s = s2 - s1
-if s < 0:
-    s += 60
-    m -= 1
-if dia <= 0:
-    dia = 0
-print('{} dia(s)\n{} hora(s)\n{} minuto(s)\n{} segundo(s)'.format(dia, h, m, s))
+h2, m2, s2 = map(int, input().split(':'))
+
+# Calcular diferencias
+total_seconds1 = (dia1 * 86400) + (h1 * 3600) + (m1 * 60) + s1
+total_seconds2 = (dia2 * 86400) + (h2 * 3600) + (m2 * 60) + s2
+
+# Obtener la diferencia total en segundos
+delta_seconds = total_seconds2 - total_seconds1
+
+# Calcular días, horas, minutos y segundos
+dia, delta_seconds = divmod(delta_seconds, 86400)
+h, delta_seconds = divmod(delta_seconds, 3600)
+m, s = divmod(delta_seconds, 60)
+
+# Imprimir resultados
+print(f"{dia} dia(s)\n{h} hora(s)\n{m} minuto(s)\n{s} segundo(s)")
