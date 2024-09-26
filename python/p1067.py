3f626f268954fd489b0970bef5efe59dce18d9ac
Sergio Majé <smajefranco@gmail.com>
2024-09-26 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1067 in Python

--- python/p1067.py
+++ python/p1067.py
@@ -1,5 +1,10 @@
-x=int(input())
-if 1<=x<=1000:
-    for i in range(1,x+1):
-        if i%2!=0:
-            print(i)
+"""Beecrowd exercise 1067.
+
+See: https://judge.beecrowd.com/es/problems/view/1067
+"""
+
+x = int(input())
+
+if 1 <= x <= 1000:
+    for i in range(1, x + 1, 2):
+        print(i)
