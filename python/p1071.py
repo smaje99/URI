5a482cba16ce6de3e28d73a7abbd3938795fd757
Sergio Majé <smajefranco@gmail.com>
2024-09-28 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1071 in Python

--- python/p1071.py
+++ python/p1071.py
@@ -1,11 +1,18 @@
+"""Beecrowd exercise 1071.
+
+See: https://judge.beecrowd.com/es/problems/view/1071
+"""
+
+
 x = int(input())
 y = int(input())
-x = max(x, y)
-y = min(x, y)
-n = 0
-b = False
-for i in range(y, x):
-    if b and i % 2 != 0:
-        n += i
-    b = True
-print(n)
+
+min_value = min(x, y) + 1
+max_value = max(x, y)
+
+start = min_value + 1 if min_value % 2 == 0 else min_value
+end = max_value
+
+sum_odd = sum(range(start, end, 2))
+
+print(sum_odd)
