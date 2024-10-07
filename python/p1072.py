068d92609a69048b82c8b3f276a91c4885e81224
Sergio Majé <smajefranco@gmail.com>
2024-10-07 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1072 in Python

--- python/p1072.py
+++ python/p1072.py
@@ -1,12 +1,21 @@
+"""Beecrowd exercise 1072.
+
+See: https://judge.beecrowd.com/es/problems/view/1072
+"""
+
+
 n = int(input())
-if n < 10000:
-    inte = 0
-    ninte = 0
-    for i in range(n):
+
+if n < 10_000:
+    inside_range = 0
+    outside_range = 0
+
+    for _ in range(n):
         x = int(input())
-        if (-10 ** 7) <= x <= (10 ** 7):
+        if -10 ** 7 <= x <= 10 ** 7:
             if 10 <= x <= 20:
-                inte += 1
+                inside_range += 1
             else:
-                ninte += 1
-    print(inte, 'in\n' + repr(ninte), 'out')
+                outside_range += 1
+
+    print(f"{inside_range} in\n{outside_range} out")
