bed06c78400a687068e6f542eb0b1ac370e3a4d2
Sergio Majé <smajefranco@gmail.com>
2024-10-13 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1074 in Python

--- python/p1074.py
+++ python/p1074.py
@@ -1,15 +1,18 @@
+"""Beecrowd exercise 1074.
+
+See: https://judge.beecrowd.com/es/problems/view/1074
+"""
+
 n = int(input())
+
 if n < 10000:
-    for i in range(n):
+    for _ in range(n):
         x = int(input())
-        if -10 ** 7 < x < 10 ** 7:
+
+        if -(10**7) < x < 10**7:
             if x == 0:
                 print("NULL")
-            elif x > 0 and x % 2 == 0:
-                print("EVEN POSITIVE")
-            elif x < 0 and x % 2 == 0:
-                print("EVEN NEGATIVE")
-            elif x > 0 and x % 2 != 0:
-                print("ODD POSITIVE")
             else:
-                print("ODD NEGATIVE")
+                parity = "EVEN" if x % 2 == 0 else "ODD"
+                sign = "POSITIVE" if x > 0 else "NEGATIVE"
+                print(f"{parity} {sign}")
