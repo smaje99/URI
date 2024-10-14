bdb99abda70cc88978350d43cda893469fdc7788
Sergio Majé <smajefranco@gmail.com>
2024-10-14 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1075 in Python

--- python/p1075.py
+++ python/p1075.py
@@ -1,5 +1,10 @@
-n=int(input())
-if n<10000:
-    for i in range(1,10001):
-        if i%n==2:
-            print(i)
+"""Beecrowd exercise 1075.
+
+See: https://judge.beecrowd.com/es/problems/view/1075
+"""
+
+n = int(input())
+
+if 1 < n < 10000:  # Ajustamos el rango para que esté entre 1 y 10000
+    for i in range(2, 10000, n):  # Comenzamos desde 2 y saltamos de n en n
+        print(i)
