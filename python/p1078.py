e0d28f57df30b6f9a54f383242064bf35eb2e15e
Sergio Majé <smajefranco@gmail.com>
2024-10-20 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1078 in Python

--- python/p1078.py
+++ python/p1078.py
@@ -1,4 +1,9 @@
-n=int(input())
-if 1<n<1000:
-    for i in range(10):
-        print(repr(i+1)+" x "+repr(n)+" = "+repr((i+1)*n))
+"""Beecrowd exercise 1078.
+
+See: https://judge.beecrowd.com/es/problems/view/1078
+"""
+
+n = int(input())
+if 1 < n < 1000:
+    for i in range(1, 11):
+        print(f"{i} x {n} = {i * n}")
