deba06113e714c4628d6fcf20ac56e4a0f246540
Sergio Majé <smajefranco@gmail.com>
2024-10-22 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1080 in Python

--- python/p1080.py
+++ python/p1080.py
@@ -1,12 +1,16 @@
-i=0
-l=[]
-alto=0
-ind=i
-while i!=100:
-    b=int(input())
-    i+=1
-    if b>alto:
-        alto=b
-        ind=i
-print(alto)
-print(ind)
+"""Beecrowd exercise 1080.
+
+See: https://judge.beecrowd.com/es/problems/view/1080
+"""
+
+max_value = 0
+index_of_max = 0
+
+for i in range(1, 101):
+    value = int(input())
+    if value > max_value:
+        max_value = value
+        index_of_max = i
+
+print(max_value)
+print(index_of_max)
