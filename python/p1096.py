8fdf215e626dcbc3d4c6aa364b1a974d75d9643d
Sergio Majé <smajefranco@gmail.com>
2024-10-25 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1096 in Python

--- python/p1096.py
+++ python/p1096.py
@@ -3,15 +3,16 @@
 See: https://judge.beecrowd.com/es/problems/view/1096
 """
 
+import itertools
+
 
 def main():
     """Main function."""
     i_iter = range(1, 10, 2)
     j_iter = range(7, 4, -1)
 
-    for i in i_iter:
-        for j in j_iter:
-            print(f"I={i} J={j}")
+    for i, j in itertools.product(i_iter, j_iter):
+        print(f"I={i} J={j}")
 
 
 if __name__ == "__main__":
