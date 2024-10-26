18b1ef4bcfc49e2ba684cbbcaae9cc414627b527
Sergio Majé <smajefranco@gmail.com>
2024-10-26 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1098 in Python

--- python/p1098.py
+++ python/p1098.py
@@ -3,20 +3,21 @@
 See: https://judge.beecrowd.com/es/problems/view/1098
 """
 
+import itertools
+
 
 def main():
     """Main function."""
     i_seq = range(0, 22, 2)
     j_seq = range(1, 4)
 
-    for i in i_seq:
-        for j in j_seq:
-            if i % 10 == 0:
-                i_num = i // 10
-                print(f"I={i_num} J={j + i_num}")
-            else:
-                i_num = i / 10
-                print(f"I={i_num:.1f} J={j + i_num:.1f}")
+    for i, j in itertools.product(i_seq, j_seq):
+        if i % 10 == 0:
+            i_num = i // 10
+            print(f"I={i_num} J={j + i_num}")
+        else:
+            i_num = i / 10
+            print(f"I={i_num:.1f} J={j + i_num:.1f}")
 
 
 if __name__ == "__main__":
