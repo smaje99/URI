2f5ee64e729def6c759cbf273cd220cbe4019679
Sergio Majé <smajefranco@gmail.com>
2024-11-02 18:42:13 -0500

    🎨 Format solution for Beecrowd exercise 1103 in Python

--- python/p1103.py
+++ python/p1103.py
@@ -1,10 +1,18 @@
+"""Beecrowd exercise 1103.
+
+See: https://judge.beecrowd.com/es/problems/view/1103
+"""
+
 while True:
     H1, M1, H2, M2 = map(int, input().split())
+
     if H1 == M1 == H2 == M2 == 0:
         break
-    else:
-        x = H1 * 60 + M1
-        y = H2 * 60 + M2
-        if y <= x:
-            y += 24 * 60
-        print(abs(y - x))
+
+    x = H1 * 60 + M1
+    y = H2 * 60 + M2
+
+    if y <= x:
+        y += 24 * 60
+
+    print(abs(y - x))
