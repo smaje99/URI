388cd4baeb3ad6ff14211f6bad95bbb39b0d8798
Sergio Majé <smajefranco@gmail.com>
2024-11-07 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1114 in Python

--- python/p1114.py
+++ python/p1114.py
@@ -1,6 +1,12 @@
+"""Beecrowd exercise 1114.
+Repetition
+
+See: https://judge.beecrowd.com/es/problems/view/1114
+"""
+
 while True:
-    n=input()
-    if n=='2002':
+    n = input().strip()
+    if n == "2002":
         print("Acesso Permitido")
         break
     print("Senha Invalida")
