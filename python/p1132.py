122f662ecf65dcd8834bfdf5c7b4d11ea04c419f
Sergio Majé <smajefranco@gmail.com>
2024-12-22 19:41:42 -0500

    🎨 Format solution for Beecrowd exercise 1132 in Python

--- python/p1132.py
+++ python/p1132.py
@@ -1,7 +1,15 @@
+"""Beecrowd exercise 1120.
+Multiples of 13.
+Repetition
+
+See: https://judge.beecrowd.com/es/problems/view/1120
+"""
+
 x = int(input())
 y = int(input())
-j = 0
-for i in range(min(x, y), max(x, y) + 1):
-    if i % 13 != 0:
-        j += i
-print(j)
+
+sum_of_non_multiples_of_13 = sum(
+  multiple for multiple in range(min(x, y), max(x, y) + 1) if multiple % 13 != 0
+)
+
+print(sum_of_non_multiples_of_13)
