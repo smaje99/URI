172a1c70cbddb5749ed3ff3f19f1fb006f916058
Sergio Majé <smajefranco@gmail.com>
2024-12-26 12:00:00 -0500

    ✨ Add solution for Beecrowd exercise 2615 in SQL

---
+++ sql/sql2615.sql
@@ -0,0 +1,11 @@
+/*
+ * Beecrowd exercise 2615.
+ * Expanding the Business.
+ * Data selection
+ * https://www.urionlinejudge.com.br/judge/problems/view/2615
+ */
+
+select distinct
+  c.city as city
+from
+  customers c;
