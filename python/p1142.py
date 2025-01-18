588bc5ab791173bb58a2b0864bc5ccb9721e1663
Sergio Majé <smajefranco@gmail.com>
2025-01-18 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1142 in Python

--- python/p1142.py
+++ python/p1142.py
@@ -1,9 +1,22 @@
-n=int(input())
-a=1
-for i in range(n):
-    for j in range(4):
-        if j==3:
-            print("PUM")
+"""Beecrowd exercise 1142.
+PUM.
+Repetition.
+
+See: https://judge.beecrowd.com/es/problems/view/1142
+"""
+
+from itertools import product
+
+
+def main():
+    """Main function."""
+    n = int(input())
+    for a, _ in enumerate(product(range(n), range(4)), start=1):
+        if a % 4 != 0:
+            print(a, end=" ")
         else:
-            print(a,"",end="")
-        a+=1
+            print("PUM")
+
+
+if __name__ == "__main__":
+    main()
