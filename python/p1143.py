7a3a3f6532f357f9a169328e7fa5709b85d28ad5
Sergio Majé <smajefranco@gmail.com>
2025-01-23 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1143 in Python

--- python/p1143.py
+++ python/p1143.py
@@ -1,4 +1,18 @@
-n=int(input())
-if  1<n<1000:
-    for i in range(0,n):
-        print(str(i+1)+" "+str(((i+1)**2))+" "+str(((i+1)**3)))
+"""Beecrowd exercise 1143.
+Squared and Cubic.
+Repetition.
+
+See: https://judge.beecrowd.com/es/problems/view/1143
+"""
+
+
+def main():
+    """Main function."""
+    n = int(input())
+    if 1 < n < 1000:
+        for i in range(1, n + 1):
+            print(f"{i} {i ** 2} {i ** 3}")
+
+
+if __name__ == "__main__":
+    main()
