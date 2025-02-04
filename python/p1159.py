e4b4fd4332c5aa804c45056a1c82faafcf07698e
Sergio Majé <smajefranco@gmail.com>
2025-02-04 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1159 in Python

--- python/p1159.py
+++ python/p1159.py
@@ -1,20 +1,27 @@
-l = []
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    elif n % 2 == 0:
-        m = n
-        for i in range(4):
-            m += 2
-            n += m
-    else:
-        n += 1
-        m = n
-        for i in range(4):
-            m += 2
-            n += m
-    l.append(n)
+"""Beecrowd exercise 1159.
+Sum of Consecutive Even Numbers.
+Repetition.
 
-for i in l:
-    print(i)
+See: https://judge.beecrowd.com/es/problems/view/1159
+"""
+
+
+def main():
+    """Main function."""
+    while True:
+        n = int(input())
+
+        if n == 0:
+            break
+
+        if n % 2 != 0:
+            n += 1  # Make n even
+
+        # n + (n+2) + (n+4) + (n+6) + (n+8) = 5n + 20
+        total = 5 * n + 20
+
+        print(total)
+
+
+if __name__ == "__main__":
+    main()
