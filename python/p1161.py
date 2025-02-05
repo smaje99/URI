dda3606ddb50bc9d0089a13d3636d714d29bf7bc
Sergio Majé <smajefranco@gmail.com>
2025-02-05 12:00:00 -0500

    🎨 Format solution for Beecrowd exercise 1161 in Python

--- python/p1161.py
+++ python/p1161.py
@@ -1,9 +1,24 @@
-from math import factorial as fac
-l=[]
-try:
-    while True:
-        a,b=input().split()
-        l.append(fac(int(a))+fac(int(b)))
-except EOFError as error:
-    for i in l:
-        print(i)
+"""Beecrowd exercise 1161.
+Factorial Sum.
+Math, factorial.
+
+See: https://judge.beecrowd.com/es/problems/view/1161
+"""
+
+
+
+def main():
+    """Main function."""
+    import contextlib
+    from math import factorial
+
+    fact = [factorial(i) for i in range(21)]
+
+    with contextlib.suppress(EOFError):
+        while True:
+            a, b = map(int, input().split())
+            print(fact[a] + fact[b])
+
+
+if __name__ == "__main__":
+    main()
