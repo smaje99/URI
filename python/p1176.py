1f32148de9dd8972f68f7d01f72522834656cda5
Sergio Majé <smajefranco@gmail.com>
2025-06-30 15:31:09 -0500

    🎨 Format solution for Beecrowd exercise 1176 in Python

--- python/p1176.py
+++ python/p1176.py
@@ -1,10 +1,25 @@
-fib = [0]
-a, b = 0, 1
-for i in range(60):
-    fib.append(b)
-    a, b = b, a + b
-t = int(input())
-for i in range(t):
-    n = int(input())
-    if 0 <= n <= 60:
-        print("Fib({0}) = {1}".format(n, fib[n]))
+"""Beecrowd exercise 1176.
+Arreglo de Fibonacci.
+Beginner.
+Array.
+
+See: https://judge.beecrowd.com/es/problems/view/1176
+"""
+
+
+def main():
+    """Main function."""
+
+    # Fibonacci serie via dynamic programming.
+    # Precompute Fibonacci numbers up to Fib(60).
+    fib = [0, 1]
+    fib.extend(fib[i - 1] + fib[i - 2] for i in range(2, 61))
+
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        print(f"Fib({n}) = {fib[n]}")
+
+
+if __name__ == "__main__":
+    main()
