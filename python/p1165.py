6f5504b933672e3f8454a0fca70556a9e5e47de6
Sergio Majé <smajefranco@gmail.com>
2025-06-30 21:31:51 -0500

    🎨 Format solution for Beecrowd exercise 1165 in Python

--- python/p1165.py
+++ python/p1165.py
@@ -1,14 +1,39 @@
-import math
+"""Beecrowd exercise 1165.
+Prime Number.
+Beginner.
+Repetition.
+
+See: https://judge.beecrowd.com/es/problems/view/1165
+"""
+
+from math import isqrt
 
 
-def esPrime(n):
-    if n % 2 == 0 and n > 2: return False
-    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+def is_prime(n: int) -> bool:
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    limit = isqrt(n) + 1
+    return all(n % i != 0 for i in range(3, limit, 2))
+
+def main():
+    """Main function to read input and check for prime numbers."""
+    n = int(input())
+    if not (1 <= n <= 100):
+        return
+
+    for _ in range(n):
+        number = int(input())
+        if not (1 <= number <= 10**7):
+            continue
+
+        result = 'eh primo' if is_prime(number) else 'nao eh primo'
+        print(f'{number} {result}')
 
 
-m = int(input())
-if 1 <= m <= 100:
-    for i in range(m):
-        x = int(input())
-        if 1 < x <= 10 ** 7:
-            print(repr(x) + (' eh primo' if esPrime(x) else ' nao eh primo'))
+if __name__ == '__main__':
+    main()
