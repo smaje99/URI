7b345a7c6564cbd0aaf998a7a6cffc11c86bf6bd
Sergio Majé <smajefranco@gmail.com>
2025-06-30 21:39:22 -0500

    🎨 Format solution for Beecrowd exercise 1168 in Python

--- python/p1168.py
+++ python/p1168.py
@@ -1,22 +1,31 @@
-n = int(input())
-if 1 <= n <= 2000:  
-    l=[]
-    for i in range(n):
-        v = str(input())
-        j = 0
-        for s in v:
-            if s == '0' or s=='6' or s=='9':
-                j += 6
-            elif s == '1':
-                j += 2
-            elif s == '2' or s=='3' or s=='5':
-                j += 5
-            elif s == '4':
-                j += 4
-            elif s == '7':
-                j += 3
-            elif s == '8':
-                j += 7
-        l.append(j)
-    for i in l:
-        print(i,"leds")
+"""Beecrowd exercise 1168.
+LED.
+Beginner.
+String.
+
+See: https://judge.beecrowd.com/es/problems/view/1168
+"""
+
+
+def main():
+    """Main function to read input and calculate LED segments."""
+    led_count = {
+        '0': 6, '1': 2, '2': 5, '3': 5, '4': 4,
+        '5': 5, '6': 6, '7': 3, '8': 7, '9': 6
+    }
+
+    n = int(input())
+    if not (1 <= n <= 2000):
+        return
+
+    for _ in range(n):
+        number = input()
+        if not (1 <= len(number) <= 10**100):
+            continue
+
+        total_leds = sum(led_count[digit] for digit in number)
+        print(f'{total_leds} leds')
+
+
+if __name__ == '__main__':
+    main()
