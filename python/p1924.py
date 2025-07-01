3b5bc309ff57956678aaa19722ae7519a4021834
Sergio Majé <smajefranco@gmail.com>
2025-06-30 20:41:23 -0500

    ✨ Add solution for Beecrowd exercise 1924 in Python

---
+++ python/p1924.py
@@ -0,0 +1,19 @@
+"""Beecrowd exercise 1924.
+Vitória and Her Indecision.
+Beginner.
+Unknown.
+
+See: https://judge.beecrowd.com/es/problems/view/1924
+"""
+
+
+def main():
+    n = int(input())
+    for _ in range(n):
+        _ = input()
+
+    print('Ciencia da Computacao')
+
+
+if __name__ == '__main__':
+    main()
